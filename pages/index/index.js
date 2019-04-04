362f9d35a6741fcfcc05667441b6b4a55210129a
bovink <bovink@sina.com>
2019-04-04 16:08:29 +0800

    修改Index页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,7 @@
 //index.js
-//获取应用实例
-const item = ['red', 'blue', 'yellow', 'black', 'green']
 Page({
   data: {
-    toView: 'red'
-  },
-  upper: function (e) {
-    console.log(e)
-  },
-  lower: function (e) {
-    console.log(e)
-  },
-  scroll: function (e) {
-    console.log(e)
-  },
-  tap: function(e) {
-    for (let i = 0;i < item.length;i++) {
-      if (item[i] == this.data.toView) {
-        let index = i == item.length - 1 ? 0 : i + 1
-        this.setData({
-          toView: item[index]
-        })
-        break
-      }
-    }
-    
-  },
-  enterView: function() {
-    wx.navigateTo({
-      url: '../scroll-view/scroll-view',
-    })
+    array:[1,2,3,4,5,6,7,8,9,10]
   },
   onLoad: function() {
 
