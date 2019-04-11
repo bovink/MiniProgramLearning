fafd8923b989504be6351f50eff53e3622f87115
bovink <bovink@sina.com>
2019-04-11 14:44:52 +0800

    API界面添加视图。

--- pages/api/api.js
+++ pages/api/api.js
@@ -5,7 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    array: [
+      'scroll-view',
+      'video',
+      'button',
+      'checkbox'
+    ]
+  },
+  enterView: function (e) {
+    console.log(e.currentTarget.dataset.text)
+    let text = e.currentTarget.dataset.text
+    let name = '../' + text + '/' + text
+    wx.navigateTo({
+      url: name,
+    })
   },
 
   /**
