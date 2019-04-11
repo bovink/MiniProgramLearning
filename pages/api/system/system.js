e2845c646ad10e71c4989ef8d59ef3b0e3daabbc
bovink <bovink@sina.com>
2019-04-11 16:18:05 +0800

    Update system.js

--- pages/api/system/system.js
+++ pages/api/system/system.js
@@ -11,56 +11,44 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getSystemInfo()
+    this.getSystemInfoSync()
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 异步获取系统信息
    */
-  onReady: function () {
-
+  getSystemInfo: function() {
+    wx.getSystemInfo({
+      success(res) {
+        console.log(res.model)
+        console.log(res.pixelRatio)
+        console.log(res.windowWidth)
+        console.log(res.windowHeight)
+        console.log(res.language)
+        console.log(res.version)
+        console.log(res.platform)
+      }
+    })
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 同步获取系统信息
    */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getSystemInfoSync: function() {
+    try {
+      const res = wx.getSystemInfoSync()
+      console.log(res.model)
+      console.log(res.pixelRatio)
+      console.log(res.windowWidth)
+      console.log(res.windowHeight)
+      console.log(res.language)
+      console.log(res.version)
+      console.log(res.platform)
+    } catch (e) {
+      // Do something when catch error
+    }
   }
+
 })
